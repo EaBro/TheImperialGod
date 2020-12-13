b04c178df046b600149ab0c05267be80d8877832
NightZan999 <nigamanth.srivatsan@gmail.com>
2020-12-13 20:34:47 +0530

    Greatest update w/ cogs
    Added cogs for literally everything.

---
+++ TheImperialGod/cogs/economy.py
@@ -0,0 +1,273 @@
+import discord
+from discord import Embed
+from discord.ext import commands
+import json
+from discord.ext.commands import cooldown
+from discord.ext.commands import BucketType
+import random
+
+class Economy(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+    
+    async def get_bank_data(self):
+        with open("./data/mainbank.json", "r") as f:
+            users = json.load(f)
+        return users
+    
+    async def open_account(self, ctx):
+        users = await self.get_bank_data()
+        if str(user.id) in users:
+            return False
+        else:
+            users[str(user.id)] = {}
+            users[str(user.id)]["wallet"] = 0
+            users[str(user.id)]["bank"] = 0
+            return True
+
+        with open("./data/mainbank.json", "w") as f:
+            json.dump(users,f)
+
+    async def update_bank(self, user, change = 0, mode = 'wallet'):
+        users = await self.get_bank_data()
+        await self.open_account(user)
+
+        users[str(user.id)][mode] += change
+
+        with open("./data/mainbank.json", "w") as f:
+            json.dump(users,f)
+    
+    @commands.command(aliases=["bal"])
+    @cooldown(1, 5, BucketType.user)
+    async def balance(self, ctx, user : discord.Member = None):
+        if user == None:
+            user = ctx.author
+        users = await self.get_bank_data()
+        await self.open_account(user)
+
+        wallet_amt = users[str(user.id)]["wallet"]
+        bank_amt = users[str(user.id)]["bank"]
+
+        em = discord.Embed(title = f"{user.name}'s Balance",color = ctx.author.color)
+        em.add_field(name = "Wallet Balance:", value = f"{wallet_amt} :coin:")
+        em.add_field(name = "Bank Balance:", value = f"{bank_amt} :coin:")
+        await ctx.send(embed = em)
+    
+    @commands.command()
+    @cooldown(1, 15, BucketType.user)
+    async def beg(self, ctx):
+        users = await self.get_bank_data()
+        await self.open_account(ctx.author)
+        earnings = random.randint(1, 100)
+        await self.update_bank(ctx.author, earnings)
+
+        wallet_amt = users[str(ctx.author.id)]["wallet"]
+        bank_amt = users[str(ctx.author.id)]["bank"]
+
+        em = discord.Embed(title = f"{ctx.author.name} begs",color = ctx.author.color)
+        em.add_field(name = "Wallet Balance:", value = f"{wallet_amt} :coin:")
+        em.add_field(name = "Bank Balance:", value = f"{bank_amt} :coin:")
+        em.add_field(name = f"Amount Earned:", value = f"{earnings} :coin:", inline = False)
+        await ctx.send(embed = em)
+    
+    @beg.error
+    async def beg_error(self, ctx, error):
+        if isinstance(error, commands.CommandOnCooldown):
+            em = Embed(title = "<:fail:761292267360485378> Beg Error", color = ctx.author.color)
+            em.add_field(name = 'Reason:', value = 'Stop begging so much, makes you look poor!')
+            em.add_field(name = "Try again in:", value = "{:.2}s".format(error.retry_after))
+            await ctx.send(embed = em)
+
+    @commands.command(aliases=["with"])
+    @cooldown(1,3, BucketType.user)
+    async def withdraw(self, ctx, amount = None):
+        if amount == None:
+            await ctx.send("Type an amount!")
+            return
+        
+        users = await self.get_bank_data()
+        await self.open_account(ctx.author)
+        if amount == 0 or amount < 0:
+            await ctx.send("Amount must be positive!")
+            return
+
+        if amount > users[str(ctx.author.id)]["bank"]:
+            await ctx.send("You don't even have that much money!")
+            return
+
+        await self.update_bank(ctx.author, amount)
+        await self.update_bank(ctx.author, -1*amount, "bank")
+
+        wallet_amt = users[str(ctx.author.id)]["wallet"]
+        bank_amt = users[str(ctx.author.id)]["bank"]
+        em = discord.Embed(title = f"{ctx.author.name} withdraws!",color = ctx.author.color)
+        em.add_field(name = "Wallet Balance:", value = f"{wallet_amt} :coin:")
+        em.add_field(name = "Bank Balance:", value = f"{bank_amt} :coin:")
+        em.add_field(name = f"Amount Withdrawn:", value = f"{amount} :coin:", inline = False)
+        await ctx.send(embed = em)
+
+    @withdraw.error
+    async def withdraw_error(self, ctx, error):
+        if isinstance(error, commands.CommandOnCooldown):
+            em = Embed(title = "<:fail:761292267360485378> Withdraw Error", color = ctx.author.color)
+            em.add_field(name = 'Reason:', value = 'Stop withdrawing so much, makes you look like a tax accountant!')
+            em.add_field(name = "Try again in:", value = "{:.2}s".format(error.retry_after))
+            await ctx.send(embed = em)
+
+    @commands.command(aliases=["dep"])
+    @cooldown(1, 3, BucketType.user)
+    async def deposit(self, ctx, amount = None):
+        if amount == None:
+            await ctx.send("Type an amount!")
+            return
+        
+        users = await self.get_bank_data()
+        await self.open_account(ctx.author)
+        if amount == 0 or amount < 0:
+            await ctx.send("Amount must be positive!")
+            return
+
+        if amount > users[str(ctx.author.id)]["wallet"]:
+            await ctx.send("You don't even have that much money!")
+            return
+
+        await self.update_bank(ctx.author, -1*amount)
+        await self.update_bank(ctx.author, amount, "bank")
+
+        wallet_amt = users[str(ctx.author.id)]["wallet"]
+        bank_amt = users[str(ctx.author.id)]["bank"]
+        em = discord.Embed(title = f"{ctx.author.name} deposits!",color = ctx.author.color)
+        em.add_field(name = "Wallet Balance:", value = f"{wallet_amt} :coin:")
+        em.add_field(name = "Bank Balance:", value = f"{bank_amt} :coin:")
+        em.add_field(name = f"Amount Deposited:", value = f"{amount} :coin:", inline = False)
+        await ctx.send(embed = em)
+    
+    @deposit.error
+    async def deposit_error(self, ctx, error):
+        if isinstance(error, commands.CommandOnCooldown):
+            em = Embed(title = "<:fail:761292267360485378> Deposit Error", color = ctx.author.color)
+            em.add_field(name = 'Reason:', value = 'Stop depositing so much, makes you look like a tax accountant!')
+            em.add_field(name = "Try again in:", value = "{:.2}s".format(error.retry_after))
+            await ctx.send(embed = em)
+    
+    @commands.command()
+    @cooldown(1, 30, BucketType.user)
+    async def slots(self, ctx, amount = None):
+        if amount == None:
+            await ctx.send("Type an amount!")
+            return
+        
+        users = await self.get_bank_data()
+        await self.open_account(ctx.author)
+        if amount == 0 or amount < 0:
+            await ctx.send("Amount must be positive!")
+            return
+
+        if amount > users[str(ctx.author.id)]["wallet"]:
+            await ctx.send("You don't even have that much money!")
+            return
+
+        final = []
+        for i in range(0, 3):
+            a = random.choice(["👻", "👾", "🔱"])
+            final.append(a)
+        
+        if final[0] == final[1] or final[1] == final[2] or final[0] == final[2]:
+            winTrue = True
+            await self.update_bank(ctx.author, amount)
+        else:
+            winTrue = False
+            await self.update_bank(ctx.author, -1*amount)
+        
+        wallet_amt = users[str(ctx.author.id)]["wallet"]
+        bank_amt = users[str(ctx.author.id)]["bank"]
+        em = discord.Embed(title = f"{ctx.author.name} bets!",color = ctx.author.color)
+        em.add_field(name = "Wallet Balance:", value = f"{wallet_amt} :coin:")
+        em.add_field(name = "Bank Balance:", value = f"{bank_amt} :coin:")
+        em.add_field(name = f"Amount bet:", value = f"{amount} :coin:", inline = False)
+        em.add_field(name = "Result:", value = str(final))
+        if winTrue:
+            em.add_field(name = "Profit:", value = f"{amount} :coin:")
+        else:
+            em.add_field(name = "Loss", value = f"{amount} :coin:")
+        await ctx.send(embed = em)
+    
+    @slots.error
+    async def slots_error(self, ctx, error):
+        if isinstance(error, commands.CommandOnCooldown):
+            em = Embed(title = "<:fail:761292267360485378> Slots Error", color = ctx.author.color)
+            em.add_field(name = 'Reason:', value = 'If you would then you would lose all your money!')
+            em.add_field(name = "Try again in:", value = "{:.2}s".format(error.retry_after))
+            await ctx.send(embed = em)
+
+    @commands.command(aliases=["share", "send"])
+    @cooldown(1, 10, BucketType.user)
+    async def give(self, ctx, member : discord.Member = None,amount = None):
+        if amount == None:
+            await ctx.send("Type an amount!")
+            return
+        if member == None:
+            await ctx.send("Type a person to send money to!")
+            return
+        
+        users = await self.get_bank_data()
+        await self.open_account(ctx.author)
+        await self.open_account(member)
+        if amount == 0 or amount < 0:
+            await ctx.send("Amount must be positive!")
+            return
+
+        if amount > users[str(ctx.author.id)]["wallet"]:
+            await ctx.send("You don't even have that much money!")
+            return
+
+        await self.update_bank(ctx.author, -1*amount)
+        await self.update_bank(member, amount)
+
+    @give.error
+    async def give_error(self, ctx, error):
+        if isinstance(error, commands.CommandOnCooldown):
+            em = Embed(title = "<:fail:761292267360485378> Give Error", color = ctx.author.color)
+            em.add_field(name = 'Reason:', value = 'If you give all your money, you would become poor!')
+            em.add_field(name = "Try again in:", value = "{:.2}s".format(error.retry_after))
+            await ctx.send(embed = em)
+
+    @commands.command(aliases=["steal", "walletrob"])
+    @cooldown(1, 60, BucketType.user)
+    async def rob(self, ctx, member : discord.Member = None):
+        if member == None:
+            await ctx.send("Type a person to rob!")
+            return
+        
+        users = await self.get_bank_data()
+        await self.open_account(ctx.author)
+        await self.open_account(member)
+
+        if users[str(ctx.author.id)]["wallet"] < 500:
+            await ctx.send("You don't even 500 coins!")
+            return
+        if users[str(member.id)]["wallet"] < 500:
+            await ctx.send("Victim doesn't have 500 coins not worth it!")
+            return
+        if member.bot:
+            await ctx.send("You can't rob a bot back of my kind!")
+            return
+        
+        wallet_amt = users[str(member.id)]["wallet"]
+        amount = random.randint(1, wallet_amt)
+
+        await self.update_bank(ctx.author, amount)
+        await self.update_bank(member, -1*amount)
+
+        await ctx.send("You gained `{}` :coin: from that!".format(amount))
+
+    @rob.error
+    async def rob_error(self, ctx, error):
+        if isinstance(error, commands.CommandOnCooldown):
+            em = Embed(title = "<:fail:761292267360485378> Rob Error", color = ctx.author.color)
+            em.add_field(name = 'Reason:', value = 'Pesky robber! You need time to plan your next attack!')
+            em.add_field(name = "Try again in:", value = "{:.2}s".format(error.retry_after))
+            await ctx.send(embed = em)
+
+def setup(client):
+    client.add_cog(Economy(client))
---
+++ TheImperialGod/cogs/moderation/mod.py
@@ -0,0 +1,214 @@
+import discord
+from discord.ext import commands
+from discord.ext.commands import has_permissions
+from discord.ext.commands import MissingPermissions, BadArgument
+from asyncio import sleep
+from discord.ext.commands import cooldown, BucketType
+import random
+import json
+
+class Mod(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print("Mod commands Loaded!")
+
+    @commands.command(aliases = ["purge", "massdelete", "bulkdel"])
+    @has_permissions(manage_messages = True)
+    async def clear(self, ctx, amount = 1):
+        await ctx.channel.purge(limit = amount)
+        
+    @clear.error
+    async def clear_error(self, ctx, error):
+        if isinstance(error, MissingPermissions):
+            embed = discord.Embed(title = "<:fail:761292267360485378> Purge Failed!", color = ctx.author.color)
+            embed.add_field(name = "Reason:", value = f"`Manage Messages Permissions Missing!`")
+            await ctx.send(embed = embed)
+
+    @commands.command()
+    @has_permissions(manage_channels = True)
+    async def lock(self, ctx, *, reason = None):
+        await ctx.channel.set_permissions(ctx.guild.default_role, send_messages = False, read_messages = True)
+        em = discord.Embed(title = f"<:success:761297849475399710> Channel has been locked!", color = discord.Color.green())
+        em.add_field(name = "**Responsible Moderator:**", value = f"`{ctx.author.name}`")
+        em.add_field(name = "**Reason:**", value = f"`{reason}`")
+        em.add_field(name=  "Description", value = "You are not muted this channel is locked! No one but mods can type in this channel!", inline = False)
+        await ctx.channel.send(embed = em)    
+        
+    @lock.error
+    async def lock_error(self, ctx, error):
+        if isinstance(error, MissingPermissions):
+            embed = discord.Embed(title = "<:fail:761292267360485378> Lock Failed!", color = ctx.author.color)
+            embed.add_field(name = "Reason:", value = f"Manage Channels Permissions Missing!")
+            await ctx.send(embed = embed)
+
+    @commands.command()
+    @has_permissions(manage_channels = True)
+    async def unlock(self, ctx, *, reason = None):
+        await ctx.channel.set_permissions(ctx.guild.default_role, send_messages = True, read_messages = True)
+        em = discord.Embed(title = f"<:success:761297849475399710> Channel has been unlocked!", color = discord.Color.green())
+        em.add_field(name = "**Responsible Moderator:**", value = f"`{ctx.author.name}`")
+        em.add_field(name = "**Reason:**", value = f"`{reason}`")
+        em.add_field(name=  "Description", value = "You are not unmuted this channel is unlocked! No one but mods can type in this channel!", inline = False)
+        await ctx.channel.send(embed = em)    
+
+    @unlock.error
+    async def unlock_error(self, ctx, error):
+        if isinstance(error, MissingPermissions):
+            embed = discord.Embed(title = "<:fail:761292267360485378> Unlock Failed!", color = ctx.author.color)
+            embed.add_field(name = "Reason:", value = f"Manage Channels Permissions Missing!")
+            await ctx.send(embed = embed)
+
+    @commands.command()
+    @has_permissions(manage_channels = True)
+    async def setdelay(self, ctx, amount = 5, *, reason = None):
+        if amount > 6000:
+            await ctx.channel.send("Amount needs to be less than 6000!")
+            return
+
+        await ctx.channel.edit(slowmode_delay=amount)
+        em = discord.Embed(title = "<:success:761297849475399710> Change in channel settings", color = ctx.author.color)
+        em.add_field(name = "**Responsible Moderator:**", value = f"`{ctx.author.name}`")
+        em.add_field(name = "**Reason:**", value = f"`{reason}`")
+        em.add_field(name=  "Description", value = f"Now the channel has a slowmode which avoids spamming\n {ctx.author.mention} for more type `imp lock [reason]`", inline = False)
+        em.add_field(name = "Slowmode", value = f"`{amount} seconds`")
+        await ctx.send(embed = em)
+
+    @setdelay.error
+    async def setdelay_error(self, ctx, error):
+        if isinstance(error, MissingPermissions):
+            embed = discord.Embed(title = "Setdelay Failed!", color = ctx.author.color)
+            embed.add_field(name = "Reason:", value = f"Manage Channels Permissions Missing!")
+            await ctx.send(embed = embed)
+
+    @commands.command()
+    @has_permissions(ban_members = True)
+    async def unban(ctx, member : str, *, reason = None):
+        banned_users = await ctx.guild.bans()
+        member_name, member_disc = member.split("#")
+
+        for banned_entry in banned_users:
+            user = banned_entry.user
+
+            if (user.name, user.discriminator) == (member_name, member_disc):
+                await ctx.guild.unban(user)
+                embed = discord.Embed(title = f"{member_name} was unbanned!", color = ctx.author.color)
+                embed.add_field(name = "Reason:", value = f"`{reason}`")
+                embed.add_field(name = "Moderator:", value = f"`{ctx.author.name}`")
+                await ctx.send(embed = embed)
+                return
+
+        await ctx.send("Not a valid user, try it like this:\n`imp unban name#disc`")
+
+    #normal function
+    def convert(self, time):
+        pos = ["s","m","h","d"]
+
+        time_dict = {"s" : 1, "m" : 60, "h" : 3600 , "d" : 3600*24}
+
+        unit = time[-1]
+
+        if unit not in pos:
+            return -1
+        try:
+            val = int(time[:-1])
+        except:
+            return -2
+
+        return val * time_dict[unit]
+
+    @commands.command()
+    @has_permissions(manage_channels = True)
+    async def count(ctx, channel: discord.TextChannel = None):
+        channel = channel or ctx.channel #or since sometimes people have it locked!
+        messages = await channel.history(limit = None).flatten()
+        count = len(messages)
+
+        embed = discord.Embed(
+        title="Total Messages",
+        colour=ctx.author.color,
+        description=f"There were {count} messages in {channel.mention}")
+
+        await ctx.send(embed=embed)
+
+    @commands.command()
+    @has_permissions(kick_members = True)
+    async def kick(self, ctx, member : discord.Member = None, *, reason = None):
+        if member == None:
+            embed = discord.Embed(title = "<:fail:761292267360485378> Kick Failed!", color= ctx.author.color)
+            embed.add_field(name = "Reason:", value = "Ping a user to kick them!")
+            await ctx.send(embed = embed)
+        await member.kick(reason = reason)
+        em = discord.Embed(title = f"<:success:761297849475399710> Kick was successful!", color = ctx.author.color)
+        em.add_field(name = f"Victim:", value = f"`{member.name}`")
+        em.add_field(name = "Reason: ", value = f"`{reason}`")
+        em.add_field(name = "Moderator:", value = f"`{ctx.author.name}`")
+        await ctx.send(embed = em)
+        try:
+            await member.send(f"You were kicked in {ctx.guild.name}\nReason: `{reason}`\nModerator: `{ctx.author.name}`")
+        except:
+            pass
+
+    @kick.error
+    async def kick_error(self, ctx, error):
+        if isinstance(error, commands.MissingPermissions):
+            em = discord.Embed(title = "<:fail:761292267360485378> Kick Failed!", color = ctx.author.color)
+            em.add_field(name = "Reason:", value = "`Kick members Permission Missing!`")
+            await ctx.send(embed = em)
+        if isinstance(error, commands.BadArgument):
+            em = discord.Embed(title = "<:fail:761292267360485378> Kick Failed!", color = ctx.author.color)
+            em.add_field(name = "Reason:", value = "`Ping a user to kick them!`")
+            await ctx.send(embed = em)
+
+    @commands.command()
+    @has_permissions(ban_members = True)
+    async def ban(self, ctx, member : discord.Member = None, *,reason = None):
+        if member == None:
+            embed = discord.Embed(title = "<:fail:761292267360485378> Ban Failed!", color= ctx.author.color)
+            embed.add_field(name = "Reason:", value = "Ping a user to ban them!")
+            await ctx.send(embed = embed)
+        await member.ban(reason = reason)
+        em = discord.Embed(title = f"<:success:761297849475399710> Ban was successful!", color = ctx.author.color)
+        em.add_field(name = f"Victim:", value = f"`{member.name}`")
+        em.add_field(name = "Reason: ", value = f"`{reason}`")
+        em.add_field(name = "**Moderator**:", value = f"`{ctx.author.name}`")
+        await ctx.send(embed = em)
+        try:
+            await member.send(f"You were banned in {ctx.guild.name}\nReason: `{reason}`\nModerator: `{ctx.author.name}`")
+        except:
+            pass
+    
+    @ban.error
+    async def ban_error(self, ctx, error):
+        if isinstance(error, commands.MissingPermissions):
+            em = discord.Embed(title = "<:fail:761292267360485378> Ban Failed!", color = ctx.author.color)
+            em.add_field(name = "Reason:", value = "`Ban members Permission Missing!`")
+            await ctx.send(embed = em)
+        if isinstance(error, commands.BadArgument):
+            em = discord.Embed(title = "<:fail:761292267360485378> Ban Failed!", color = ctx.author.color)
+            em.add_field(name = "Reason:", value = "`Ping a user to Ban them!`")
+            await ctx.send(embed = em)
+
+    @commands.command()
+    @commands.has_permissions(manage_channels = True)
+    async def announce(ctx, channel : discord.TextChannel, *, msg):
+        embed = discord.Embed(title = "Announcement!", color = ctx.author.color)
+        embed.add_field(name = "Announcement:", value = f"`{msg}`")
+        embed.add_field(name = "Moderator:", value = f"`{ctx.autor.name}`")
+        await channel.send(embed = embed)
+
+    @announce.error
+    async def announce_error(ctx, error):
+        if isinstance(error, commands.MissingPermissions):
+            embed = discord.Embed(title = "Announcement failed!", color = ctx.author.color)
+            embed.add_field(name = 'Reason:', value = "Some perms are missing")
+            await ctx.send(embed = embed)
+        if isinstance(error, commands.BadArgument):
+            embed = discord.Embed(title = "Announcement failed!", color = ctx.author.color)
+            embed.add_field(name = 'Reason:', value = f"Mention a channel properly! And write a message after it!")
+            await ctx.send(embed = embed)
+
+def setup(client):
+    client.add_cog(Mod(client))
