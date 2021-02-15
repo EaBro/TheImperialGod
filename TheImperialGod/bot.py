ae1d3d7e2c7e342d1ccdb7904084197907693a83
NightZan999 <nigamanth.srivatsan@gmail.com>
2021-02-15 08:12:47 +0530

    Add custom client inside main file

--- TheImperialGod/bot.py
+++ TheImperialGod/bot.py
@@ -14,7 +14,6 @@ import random #random
 import json
 import os
 import asyncio
-from .client import TheImperialGod
 
 def load_cogs(): #loading all our cogs
     cogs = [
@@ -60,6 +59,59 @@ PUBLIC_KEY = config["publicKey"]
 BOT_PREFIX = config["prefix"]
 new_link ="https://discordapp.com/oauth2/authorize?&client_id=".join(str(CLIENT_ID))
 new_link.join("&scope=bot&permissions=21474836398")
+
+# custom client
+class TheImperialGod(commands.Bot):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.cogs = [
+            "cogs.info.help", # help command
+            "cogs.fun.animals", # searching reddit
+            "cogs.economy.bankcommands", # bank commands in economy
+            "cogs.economy.moneymaking", # moneymaking commands in economy
+            "cogs.economy.shop", # making a shop with database in economy!
+            "cogs.economy.gambling", # gambling commands
+            "cogs.fun.misc", # misc commands
+            "cogs.fun.utils", # utilities
+            "cogs.info.info", # information
+            "cogs.info.math", # math commands
+            "cogs.moderation.giveaways", # giveaway commands!
+            "cogs.moderation.mod", # moderation commands
+            "cogs.moderation.owner", # owner commands
+            "cogs.tickets.tickets", # ticket commands
+            "cogs.info.topgg", # has top.gg stuff bois!
+            "cogs.exclusive.exclusive" # has exclusive commands
+        ]
+
+        self.events = [
+            "events.GuildEvents", # when the bot leaves or joins a guild!
+            "events.ReactionAdd",
+            "events.ReactionRemove"
+        ]
+
+    async def on_ready(self):
+        print("Ready!")
+        print("Username: ", client.user.name)
+        print("User ID: ", client.user.id)
+        print("----------------------------")
+
+    async def get_cogs(self):
+        return self.cogs
+    
+    async def get_events(self):
+        return self.events
+    
+    async def get_all_emojis(self):
+        return Emojis
+    
+    async def read_json(self, filename):
+        with open(filename, "r") as f:
+            data = json.load(f)
+        return data
+    
+    async def write_json(self, output, filename):
+        with open(filename, "w") as f:
+            return json.dump(output, f)
 
 intents = discord.Intents.all()
 client = TheImperialGod(command_prefix = BOT_PREFIX, case_insensitive = True, intents = intents) #making a client object
